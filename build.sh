df05e76159a4cbbbc57ff5ec8224ae9a7cc24775
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2026-01-07 09:50:44 +0100

    Updated build.sh for docs.gymkirchenfeld.ch

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-#export NODE_OPTIONS=--openssl-legacy-provider
-yarn build #&& rsync -av --delete ./docs/.vitepress/dist/* web4.kinet.ch:/srv/www/dooku
+#!/bin/bash
+set -e
+yarn build
+
+SERVER=web4.kinet.ch
+SERVER_DIR=/srv/www/docs.gymkirchenfeld.ch/pensen/
+LOCAL_DIR=./docs/.vitepress/dist/
+ME=$(ssh ${SERVER} echo \$\{USER\})
+ssh ${SERVER} sudo chown -R ${ME} ${SERVER_DIR}
+rsync -av --delete ${LOCAL_DIR}/* ${SERVER}:${SERVER_DIR}
+ssh ${SERVER} sudo chgrp -R adm ${SERVER_DIR}
+ssh ${SERVER} sudo chmod -R g+w ${SERVER_DIR}
